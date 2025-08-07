add8453715db8fd0654be0be254a5eab0dd7f650
Programação em Python 2025.1 <62129532025.1@senacrjedu.local>
2025-08-06 21:14:29 -0300

    atualizacao 2

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Curso
 
-# Register your models here.
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+
+admin.site.register(Curso, CursoAdmin)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Usuario
 
-# Create your views here.
+def home(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
+
+def detalhes(request, usuario_id):
+    usuario = get_object_or_404(usuario, id=usuario_id)
+    return render(request, 'usuarios/detalhe_usuario.html', {'usuario': usuario})
