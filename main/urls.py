cb8a73d0b09e6989107c06796c25c317a364ed04
Programação em Python 2025.1 <62129532025.1@senacrjedu.local>
2025-08-13 20:44:52 -0300

    implementação da página login e home

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', include('usuarios.urls')), 
     path('cursos/', include('cursos.urls')), 
 ]
