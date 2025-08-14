86be212c059d3bd9984859edaecb93504d246e22
Programação em Python 2025.1 <62129532025.1@senacrjedu.local>
2025-08-13 21:41:59 -0300

    planos

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('usuarios.urls')), 
-    path('cursos/', include('cursos.urls')), 
+    path('', include('usuarios.urls')), 
+    path('cursos/', include('cursos.urls')),
 ]
